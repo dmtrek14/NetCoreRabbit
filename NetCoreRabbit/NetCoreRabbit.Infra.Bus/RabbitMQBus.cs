5ef6617d082966f2ba6e990dc5bd5b9e740af6fb
Danielle <dmayabb@lcb.state.nv.us>
2021-02-20 12:45:59 -0800

    Add rabbitmq bus

---
+++ NetCoreRabbit/NetCoreRabbit.Infra.Bus/RabbitMQBus.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using NetCoreRabbit.Domain.Core.Bus;
+using NetCoreRabbit.Domain.Core.Commands;
+using NetCoreRabbit.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace NetCoreRabbit.Infra.Bus
+{
+	public sealed class RabbitMQBus : IEventBus
+	{
+		private readonly IMediator _mediator;
+
+		public Task SendCommand<T>(T command) where T : Command
+		{
+			return _mediator.Send(command);
+		}
+
+		public void Publish<T>(T @event) where T : Event
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Subscribe<T, TH>()
+			where T : Event
+			where TH : IEventHandler<T>
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
