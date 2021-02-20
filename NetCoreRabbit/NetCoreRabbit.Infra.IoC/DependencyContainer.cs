c45ba3763f0731f7043cf22102a6ac20f24d5420
Danielle <dmayabb@lcb.state.nv.us>
2021-02-20 15:43:53 -0800

    Add dependency container

---
+++ NetCoreRabbit/NetCoreRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using NetCoreRabbit.Domain.Core.Bus;
+using NetCoreRabbit.Infra.Bus;
+
+namespace NetCoreRabbit.Infra.IoC
+{
+	public class DependencyContainer
+	{
+		public static void RegisterServices(IServiceCollection services)
+		{
+			//Domain Bus
+			services.AddTransient<IEventBus, RabbitMQBus>();
+		}
+	}
+}
