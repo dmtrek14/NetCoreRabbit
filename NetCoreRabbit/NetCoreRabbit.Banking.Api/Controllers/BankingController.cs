552e2ddd7e74014ce962aa08fc3d67f8a82c3505
Danielle <dmayabb@lcb.state.nv.us>
2021-02-21 17:53:01 -0800

    Add banking controller

---
+++ NetCoreRabbit/NetCoreRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NetCoreRabbit.Banking.Application.Interfaces;
+using NetCoreRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreRabbit.Banking.Api.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class BankingController : ControllerBase
+	{
+		private readonly IAccountService _accountService;
+		public BankingController(IAccountService accountService)
+		{
+			_accountService = accountService;
+		}
+		//GET api/banking
+		[HttpGet]
+		public ActionResult<IEnumerable<Account>> Get()
+		{
+			return Ok(_accountService.GetAccounts());
+		}
+	}
+}
